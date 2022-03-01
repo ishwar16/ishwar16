e8f529b70ccfd8fadf1296e36a808f1d339174fa
ishwar16 <48154308+ishwar16@users.noreply.github.com>
2022-03-02 03:49:22 +0530

    Add files via upload

---
+++ OIBSIP/Level2/Task3/ToDo List/main.js
@@ -0,0 +1,28 @@
+/*
+JS file of To-Do List Project
+Project : Oasis Infobyte, Web Dev, Level 2 : Task 2
+Code by Ishwar Shrinivas Gujjarwar, dated 16th Feb, 2022
+*/
+
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('li');
+
+    paragraph.classList.add('paragraph-styling');
+
+    paragraph.innerText = inputField.value;
+
+    toDoContainer.appendChild(paragraph);
+
+    inputField.value = "";
+
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
